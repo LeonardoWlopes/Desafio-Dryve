0b482d43606df366d7e53e97a225a4b617612596
Leonardo William Lopes <leonardolopessocial@gmail.com>
2023-02-15 12:32:56 -0300

    Feat: Cache On FeedBack Service

--- src/services/feedbacks.service.ts
+++ src/services/feedbacks.service.ts
@@ -6,6 +6,16 @@ export function useFeedBacks() {
     const [feedBacks, setFeedBacks] = useState<IFeedBack[]>();
     const [isLoading, setIsLoading] = useState(true);
 
+    //Normalmente eu uso a lib React Query para fetch de dados e cache, mas como não vi necessidade aqui fiz essa logica na mão mesmo
+
+    const cache = sessionStorage.getItem("dryve:feedbacks_cache");
+
+    useEffect(() => {
+        if (cache) {
+            setFeedBacks(JSON.parse(cache));
+        }
+    }, []);
+
     const isLoaded = useRef(false);
 
     useEffect(() => {
@@ -16,6 +26,10 @@ export function useFeedBacks() {
         axios
             .get("http://www.mocky.io/v2/5eb553df31000060006994a8")
             .then(({ data }) => {
+                sessionStorage.setItem(
+                    "dryve:feedbacks_cache",
+                    JSON.stringify(data)
+                );
                 setFeedBacks(data);
             })
             .finally(() => {
@@ -26,5 +40,5 @@ export function useFeedBacks() {
         isLoaded.current = true;
     }, []);
 
-    return { feedBacks, isLoading };
+    return { feedBacks, isLoading: !feedBacks ?? isLoading };
 }
